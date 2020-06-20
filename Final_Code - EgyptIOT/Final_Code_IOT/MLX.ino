01774f60880dcdd5646d076e59e457b24b19e26e
Mina Karam <38835380+Mina-Karam@users.noreply.github.com>
2020-06-20 17:06:08 +0200

    Update MLX.ino

--- Final_Code - EgyptIOT/Final_Code_IOT/MLX.ino
+++ Final_Code - EgyptIOT/Final_Code_IOT/MLX.ino
@@ -1,6 +1,6 @@
-void Get_Temp() {
-  Serial.print("Ambient = "); Serial.println(mlx.readAmbientTempC()); 
-  Cayenne.virtualWrite(1,mlx.readAmbientTempC());
-  mlxDelay.repeat();
-
-}
+//void Get_Temp() {
+//  Serial.print("Ambient = "); Serial.println(mlx.readAmbientTempC()); 
+//  Cayenne.virtualWrite(1,mlx.readAmbientTempC());
+//  mlxDelay.repeat();
+//
+//}
