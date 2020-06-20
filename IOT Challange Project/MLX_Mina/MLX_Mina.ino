9c6a1b0b82c2989a7ddf5d12357f22af9e776d68
Mina Karam <38835380+Mina-Karam@users.noreply.github.com>
2020-06-20 14:13:05 +0200

    Update MLX_Mina.ino

--- IOT Challange Project/MLX_Mina/MLX_Mina.ino
+++ IOT Challange Project/MLX_Mina/MLX_Mina.ino
@@ -11,14 +11,14 @@ Serial.begin(9600);
    mlx.begin();
   mlxDelay.start(1000);
 }
+
 void loop() {
 if(mlxDelay.justFinished()){
-  Serial.print("Ambient = "); Serial.println(mlx.readAmbientTempC()+6); 
+  Serial.print("Ambient = "); Serial.println(mlx.readAmbientTempC()); 
   //Serial.print("C\tObject = "); Serial.print(mlx.readObjectTempC()); Serial.println("C");
    if( ( mlx.readObjectTempC()<35 )|| (mlx.readObjectTempC()> 38))
   {
-   
-   Serial.println("\nHigh temp !!");  
+   Serial.println("error");  
    //Serial.println();al.print(mlx.readObjectTempF()); Serial.println("F");
   //Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempF()); 
   // Serial.print("F\tObject = "); Seri
