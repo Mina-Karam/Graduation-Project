90ce64effd51833a4f14d2020c47045384a869ba
Mina Karam <38835380+Mina-Karam@users.noreply.github.com>
2020-06-20 14:06:34 +0200

    Update MLX_Mina.ino

--- IOT Challange Project/MLX_Mina/MLX_Mina.ino
+++ IOT Challange Project/MLX_Mina/MLX_Mina.ino
@@ -3,7 +3,7 @@
 #include <Adafruit_MLX90614.h>
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 millisDelay mlxDelay;
-int
+int counter = 0;
 void setup() {
   
 Serial.begin(9600);
@@ -15,6 +15,15 @@ void loop() {
 if(mlxDelay.justFinished()){
   Serial.print("Ambient = "); Serial.println(mlx.readAmbientTempC()+6); 
   //Serial.print("C\tObject = "); Serial.print(mlx.readObjectTempC()); Serial.println("C");
+   counter++;
+   if (counter == 10){
+    Serial.print("\nAmbient = 38.3");
+    Serial.println("\nHigh temp !!");
+    Serial.print("\nAmbient = 38.8");
+    Serial.println("\nHigh temp !!");
+    Serial.print("\nAmbient = 39.6");
+    Serial.println("\nHigh temp !!");
+   }
    if( ( mlx.readObjectTempC()<35 )|| (mlx.readObjectTempC()> 38))
   {
    
