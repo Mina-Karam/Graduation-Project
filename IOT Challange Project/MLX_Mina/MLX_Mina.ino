c8a8190f0980f7a2cdea59eaa4d93184d60d564f
Mina Karam <38835380+Mina-Karam@users.noreply.github.com>
2020-06-20 14:12:55 +0200

    Update MLX_Mina.ino

--- IOT Challange Project/MLX_Mina/MLX_Mina.ino
+++ IOT Challange Project/MLX_Mina/MLX_Mina.ino
@@ -3,40 +3,27 @@
 #include <Adafruit_MLX90614.h>
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 millisDelay mlxDelay;
-int counter = 0;
+
 void setup() {
   
 Serial.begin(9600);
-  Serial.println("MLX90614");  
+  Serial.println("Adafruit MLX90614 test");  
    mlx.begin();
   mlxDelay.start(1000);
 }
 void loop() {
 if(mlxDelay.justFinished()){
-  Serial.print("Ambient = "); Serial.println(mlx.readAmbientTempC()+4); 
+  Serial.print("Ambient = "); Serial.println(mlx.readAmbientTempC()+6); 
   //Serial.print("C\tObject = "); Serial.print(mlx.readObjectTempC()); Serial.println("C");
-   counter++;
-   if (counter == 10){
-    delay(500);
-    Serial.print("\nAmbient = 38.3");
-    Serial.println("\nHigh temp !!");
-    delay(500);
-    Serial.print("\nAmbient = 38.8");
-    Serial.println("\nHigh temp !!");
-    delay(500);
-    Serial.print("\nAmbient = 39.6");
-    Serial.println("\nHigh temp !!");
-    delay(2000);
-   }
-//   if( ( mlx.readObjectTempC()<35 )|| (mlx.readObjectTempC()> 38))
-//  {
-//   
-//   Serial.println("\nHigh temp !!");  
-//   //Serial.println();al.print(mlx.readObjectTempF()); Serial.println("F");
-//  //Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempF()); 
-//  // Serial.print("F\tObject = "); Seri
-//  //delay(500);
-// }
+   if( ( mlx.readObjectTempC()<35 )|| (mlx.readObjectTempC()> 38))
+  {
+   
+   Serial.println("\nHigh temp !!");  
+   //Serial.println();al.print(mlx.readObjectTempF()); Serial.println("F");
+  //Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempF()); 
+  // Serial.print("F\tObject = "); Seri
+  //delay(500);
+ }
   mlxDelay.repeat();
 } 
 }
