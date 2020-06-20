bac060a46e4cfa3fa19412801aa8208d97ec45fa
Mina Karam <38835380+Mina-Karam@users.noreply.github.com>
2020-06-20 16:57:20 +0200

    Update GPS.ino

--- Final_Code - EgyptIOT/Final_Code_IOT/GPS.ino
+++ Final_Code - EgyptIOT/Final_Code_IOT/GPS.ino
@@ -1,1 +1,31 @@
+void Get_GPS(){
+  /********* Show in serial ***********/
 
+  Serial.print("latitude :");
+  latitude_new = (sim808.GPSdata.lat+latitude_erorr+latitude_old)/2;
+  Serial.println(latitude_new,6);
+  latitude_old = sim808.GPSdata.lat+latitude_erorr;
+  
+  Serial.print("longitude :");
+  longitude_new = (sim808.GPSdata.lon+longitude_erorr+longitude_old)/2;
+  Serial.println(longitude_new,6);
+  longitude_old = sim808.GPSdata.lon+longitude_erorr;
+  
+  /********* Upload To Cayenne ********/
+  char buffer[32];
+  buffer[0] = '[';
+  size_t offset = 1;
+  dtostrf(latitude_new, 1, 9, &buffer[offset]);
+  offset += strlen(&buffer[offset]);
+  buffer[offset++] = ',';
+  dtostrf(longitude_new, 1, 9, &buffer[offset]);
+  offset += strlen(&buffer[offset]);
+  buffer[offset++] = ',';
+  dtostrf(200, 1, 1, &buffer[offset]);
+  offset += strlen(&buffer[offset]);
+  buffer[offset++] = ']';
+  buffer[offset] = 0;
+
+  Cayenne.virtualWrite(0, buffer, "gps", "m");
+
+}
