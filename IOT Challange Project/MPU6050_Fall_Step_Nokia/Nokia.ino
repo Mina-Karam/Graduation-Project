353bf0960ae41cb9b40781b0b7d7875da689d806
Mina Karam <38835380+Mina-Karam@users.noreply.github.com>
2020-06-21 00:27:46 +0200

    Update Nokia.ino

--- IOT Challange Project/MPU6050_Fall_Step_Nokia/Nokia.ino
+++ IOT Challange Project/MPU6050_Fall_Step_Nokia/Nokia.ino
@@ -8,12 +8,12 @@ void Nokia_Setup(void){
   // initialize the display
   display.begin();
  
-  display.setContrast(50);
+  display.setContrast(65);
 
   display.clearDisplay();   
   display.display();
  
-  display.setTextSize(2);
+  display.setTextSize(1);
   display.setTextColor(BLACK, WHITE);
   display.setCursor(3, 0);
   display.println("omega 6 clock");
