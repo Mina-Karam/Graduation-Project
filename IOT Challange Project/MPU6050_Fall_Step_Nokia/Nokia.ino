30a1a8f8a1d9ee0bc0ebd136df8ac63a306af596
Mina Karam <38835380+Mina-Karam@users.noreply.github.com>
2020-06-21 02:14:10 +0200

    Update Nokia.ino

--- IOT Challange Project/MPU6050_Fall_Step_Nokia/Nokia.ino
+++ IOT Challange Project/MPU6050_Fall_Step_Nokia/Nokia.ino
@@ -21,24 +21,176 @@ void Nokia_Setup(void){
 }
 
 void Nokia_Loop(void){
+  
+  if(distance < 50){
       display.setCursor(3, 15);
       display.println("Level 1\n");
-      display.print("Step: ");
-      display.println(distance);
-      display.display();
-      
-  if(distance >= 20){
+  }
+     
+  
+  if(distance == 50){
       display.clearDisplay();
       display.setCursor(3, 15);  
       display.println("Up tp level 2"); 
       display.display();
-      delay(1000);
+      delay(500);
+      display.clearDisplay(); 
+      
+      // draw mulitple circles
+      testdrawcircle();
+      display.display();
+      delay(500);
       display.clearDisplay();
-      
+      display.fillCircle(display.width()/2, display.height()/2, 10, BLACK);
+      display.display();
+      delay(500);
+      display.clearDisplay();
+    
+      testdrawroundrect();
+      delay(500);
+      display.clearDisplay();
+    
+      testfillroundrect();
+      delay(500);
+      display.clearDisplay();
+    
+      testdrawtriangle();
+      delay(500);
+      display.clearDisplay();
+       
+      testfilltriangle();
+      delay(500);
+      display.clearDisplay();
   }
-      
+   if(distance > 50){
+      display.clearDisplay();
+      display.setCursor(3, 15);
+      display.println("Level 2\n");
+  }
+      display.print("Step: ");
+      display.println(distance);
+      display.display();
+}
+void testdrawchar(void) {
+  display.setTextSize(1);
+  display.setTextColor(BLACK);
+  display.setCursor(0,0);
+
+  for (uint8_t i=0; i < 168; i++) {
+    if (i == '\n') continue;
+    display.write(i);
+    //if ((i > 0) && (i % 14 == 0))
+      //display.println();
+  }    
+  display.display();
 }
 
+void testdrawcircle(void) {
+  for (int16_t i=0; i<display.height(); i+=2) {
+    display.drawCircle(display.width()/2, display.height()/2, i, BLACK);
+    display.display();
+  }
+}
+
+void testfillrect(void) {
+  uint8_t color = 1;
+  for (int16_t i=0; i<display.height()/2; i+=3) {
+    // alternate colors
+    display.fillRect(i, i, display.width()-i*2, display.height()-i*2, color%2);
+    display.display();
+    color++;
+  }
+}
+
+void testdrawtriangle(void) {
+  for (int16_t i=0; i<min(display.width(),display.height())/2; i+=5) {
+    display.drawTriangle(display.width()/2, display.height()/2-i,
+                     display.width()/2-i, display.height()/2+i,
+                     display.width()/2+i, display.height()/2+i, BLACK);
+    display.display();
+  }
+}
+
+void testfilltriangle(void) {
+  uint8_t color = BLACK;
+  for (int16_t i=min(display.width(),display.height())/2; i>0; i-=5) {
+    display.fillTriangle(display.width()/2, display.height()/2-i,
+                     display.width()/2-i, display.height()/2+i,
+                     display.width()/2+i, display.height()/2+i, color);
+    if (color == WHITE) color = BLACK;
+    else color = WHITE;
+    display.display();
+  }
+}
+
+void testdrawroundrect(void) {
+  for (int16_t i=0; i<display.height()/2-2; i+=2) {
+    display.drawRoundRect(i, i, display.width()-2*i, display.height()-2*i, display.height()/4, BLACK);
+    display.display();
+  }
+}
+
+void testfillroundrect(void) {
+  uint8_t color = BLACK;
+  for (int16_t i=0; i<display.height()/2-2; i+=2) {
+    display.fillRoundRect(i, i, display.width()-2*i, display.height()-2*i, display.height()/4, color);
+    if (color == WHITE) color = BLACK;
+    else color = WHITE;
+    display.display();
+  }
+}
+   
+void testdrawrect(void) {
+  for (int16_t i=0; i<display.height()/2; i+=2) {
+    display.drawRect(i, i, display.width()-2*i, display.height()-2*i, BLACK);
+    display.display();
+  }
+}
+
+void testdrawline() {  
+  for (int16_t i=0; i<display.width(); i+=4) {
+    display.drawLine(0, 0, i, display.height()-1, BLACK);
+    display.display();
+  }
+  for (int16_t i=0; i<display.height(); i+=4) {
+    display.drawLine(0, 0, display.width()-1, i, BLACK);
+    display.display();
+  }
+  delay(250);
+  
+  display.clearDisplay();
+  for (int16_t i=0; i<display.width(); i+=4) {
+    display.drawLine(0, display.height()-1, i, 0, BLACK);
+    display.display();
+  }
+  for (int8_t i=display.height()-1; i>=0; i-=4) {
+    display.drawLine(0, display.height()-1, display.width()-1, i, BLACK);
+    display.display();
+  }
+  delay(250);
+  
+  display.clearDisplay();
+  for (int16_t i=display.width()-1; i>=0; i-=4) {
+    display.drawLine(display.width()-1, display.height()-1, i, 0, BLACK);
+    display.display();
+  }
+  for (int16_t i=display.height()-1; i>=0; i-=4) {
+    display.drawLine(display.width()-1, display.height()-1, 0, i, BLACK);
+    display.display();
+  }
+  delay(250);
+
+  display.clearDisplay();
+  for (int16_t i=0; i<display.height(); i+=4) {
+    display.drawLine(display.width()-1, 0, 0, i, BLACK);
+    display.display();
+  }
+  for (int16_t i=0; i<display.width(); i+=4) {
+    display.drawLine(display.width()-1, 0, i, display.height()-1, BLACK); 
+    display.display();
+  }
+  delay(250);
+}
 void draw_text(byte x_pos, byte y_pos, char *text)
 {
   display.setCursor(x_pos, y_pos);
